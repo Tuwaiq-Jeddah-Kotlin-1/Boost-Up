4be09e8d4a97cdd01d40e7e5f1ed0a19ba4fe0d5
MahilaAlmutairi <Mahila.Almutairi.jed.k1@tuwaiq.edu.sa>
2022-01-09 11:53:26 +0300

    General Improvement

--- app/src/main/java/com/mahila/motivationalQuotesApp/model/repositories/GoQuotesAPIService.kt
+++ app/src/main/java/com/mahila/motivationalQuotesApp/model/repositories/GoQuotesAPIService.kt
@@ -4,8 +4,6 @@ import QuotableData
 import retrofit2.http.GET
 
 interface GoQuotesAPIService {
-    /* @GET("all?type=tag&val=motivational")
-     suspend fun fetchQuotes(): GoQuotesData */
 
     @GET("quotes?tags=inspirational")
     suspend fun fetchQuotes(): QuotableData
